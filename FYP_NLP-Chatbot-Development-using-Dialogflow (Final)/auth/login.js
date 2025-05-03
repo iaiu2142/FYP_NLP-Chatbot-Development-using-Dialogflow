1a897b35bb4c53a3a85991a1cbc5fcb32b52412b
Ilsa Afzaal <81191657+iaiu2142@users.noreply.github.com>
2025-05-03 20:43:28 +0500

    Add files via upload
    Signed-off-by: Ilsa Afzaal <81191657+iaiu2142@users.noreply.github.com>

---
+++ FYP_NLP-Chatbot-Development-using-Dialogflow (Final)/auth/login.js
@@ -0,0 +1,28 @@
+document.getElementById('loginForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+  
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
+  
+    try {
+        const response = await fetch('http://127.0.0.1:5000/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email: email, password: password })
+        });
+  
+        const data = await response.json();
+        if (data.success) {
+            alert('✅ Login successful!');
+            localStorage.setItem('isLoggedIn', 'true');
+            window.location.href = 'http://127.0.0.1:5500/index.html'; //
+        } else {
+            alert('❌ ' + data.message);
+        }
+        
+    } catch (error) {
+        console.error('Error:', error);
+        alert('Something went wrong while logging in.');
+    }
+  });
+  
