dba95dc7724ba3a4d6e92a3e02ef33c188e377ba
Ilsa Afzaal <81191657+iaiu2142@users.noreply.github.com>
2025-05-04 23:58:47 +0500

    Delete NLP Pharma_Backend (Final) directory
    Signed-off-by: Ilsa Afzaal <81191657+iaiu2142@users.noreply.github.com>

--- NLP Pharma_Backend (Final)/config/db_config.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def get_connection():
-    return mysql.connector.connect(
-        host="localhost",              # your database host
-        user="root",         # your MySQL username
-        password="",
-        database="pharma_bot"         # your database name
-    )
